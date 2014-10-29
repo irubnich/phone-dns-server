35ee2d5d71e442c6293a5fa0e6d75d630b4fd33f
Ilya Rubnich <ilya@Ilyas-Mac-Pro.local>
2014-10-28 20:49:58 -0400

    Add specs to check for correct db indeces

--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -1,7 +1,7 @@
 require "./lib/phone_dns/db"
 
 describe PhoneDNS::DB do
-	before :each do
+	before :all do
 		@db = PhoneDNS::DB.new
 
 		# Insert an entry
@@ -13,6 +13,10 @@ describe PhoneDNS::DB do
 		})
 	end
 
+	after :all do
+		@collection.remove()
+	end
+
 	it "works with a result" do
 		result = @db.find('example.com', 'en_US')
 		expect(result["number"]).to eql "5551112323"
@@ -21,5 +25,25 @@ describe PhoneDNS::DB do
 	it "works without a result" do
 		result = @db.find('example.com', 'nah')
 		expect(result).to be_nil
+	end
+
+	it "can still insert multiple entries for a single domain" do
+		expect {
+			@collection.insert({
+				domain: 'example.com',
+				locale: 'en_UK',
+				number: '5551231122'
+			})
+		}.to_not raise_error
+	end
+
+	it "has a unique index on the domain/locale" do
+		expect {
+			@collection.insert({
+				domain: 'example.com',
+				locale: 'en_US',
+				number: '5551112323'
+			})
+		}.to raise_error(Mongo::OperationFailure)
 	end
 end
