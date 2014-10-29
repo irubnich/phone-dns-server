8d73a5e1e14f836967a30e6f159675e41c163223
Ilya Rubnich <irubnich@gmail.com>
2014-10-28 21:54:31 -0400

    Update available mongo db for heroku

--- lib/phone_dns/db.rb
+++ lib/phone_dns/db.rb
@@ -10,11 +10,12 @@ module PhoneDNS
 			# Check for production info
 			if ENV['MONGOHQ_URL']
 				client = MongoClient.from_uri(ENV['MONGOHQ_URL'])
+				db_name = URI.parse(ENV['MONGOHQ_URL']).path.gsub(/^\//, '')
 			else
 				client = MongoClient.new
 			end
 
-			db = client['phone_dns']
+			db = (db_name) ? client[db_name] : client['phone_dns']
 			@collection = db['phone_numbers']
 		end
 
